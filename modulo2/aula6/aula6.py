6370eae1db532fdc1e425d81fc68135a8817a7f0
lucasplr <lucasriffel55@gmail.com>
2022-10-28 23:54:18 -0300

    aula 6 pt 1

---
+++ modulo2/aula6/aula6.py
@@ -0,0 +1,75 @@
+# %% [markdown]
+# Relatório de Análise V
+
+# %%
+import pandas as pd 
+
+dados = pd.read_csv('./modulo2/data/aluguel_residencial.csv', sep=';')
+
+# %%
+dados.columns.name = 'índice'
+dados
+
+# %%
+dados.isnull()
+
+# %%
+dados[dados["Valor"].isnull()]
+
+# %%
+A = dados.shape[0]
+
+dados.dropna(subset = ['Valor'], inplace=True) ##remove valores nulos
+
+B = dados.shape[0]
+
+A - B
+
+# %%
+dados[dados["Valor"].isnull()]
+
+# %% [markdown]
+# Tratamento de Dados Faltantes
+
+# %%
+dados[dados['Condominio'].isnull()].shape[0]
+
+# %%
+selecao = (dados.Tipo == "Apartamento") & (dados.Condominio.isnull())
+selecao
+
+# %%
+A = dados.shape[0]
+
+dados = dados[~selecao] #inverte o booleano
+#A = dados.shape[0]
+#dados.dropna(subset = ['Condominio'], inplace=True) ##remove valores nulos
+
+#dados.dropna(subset = ['Condominio'], inplace=True)
+
+B = dados.shape[0]
+
+A - B
+
+
+# %%
+dados[dados.Condominio.isnull()].shape[0]
+
+# %%
+#dados.fillna(0, inplace=True)
+dados = dados.fillna({'Condominio': 0, "IPTU": 0}) #substitui o NaN por 0, para preencher todos os campos
+
+
+# %%
+dados[dados.Condominio.isnull()].shape[0]
+
+# %%
+dados[dados.IPTU.isnull()].shape[0]
+
+# %%
+dados.info()
+
+# %%
+
+
+
